5e4403afed6b18ea4f345d268373062486db9fa6
yyqdemao <2591981044@qq.com>
2021-03-11 22:08:45 +0800

    Initial commit

---
+++ could_payment8001/src/main/java/com/yyqdemao/devment/could_payment8001/CouldPayment8001Application.java
@@ -0,0 +1,13 @@
+package com.yyqdemao.devment.could_payment8001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouldPayment8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouldPayment8001Application.class, args);
+    }
+
+}
---
+++ could_payment8001/src/test/java/com/yyqdemao/devment/could_payment8001/CouldPayment8001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyqdemao.devment.could_payment8001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouldPayment8001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
